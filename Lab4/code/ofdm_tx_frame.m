3d5d85469444cc6d4eb0ef9768982e12240fca44
Michael Heiniger <michael.heiniger@epfl.ch>
2016-10-12 11:51:42 +0200

    ofdm_tx_frame coded, not tested

--- Lab4/code/ofdm_tx_frame.m
+++ Lab4/code/ofdm_tx_frame.m
@@ -16,4 +16,43 @@ function [tx_signal] = ofdm_tx_frame(num_carriers, num_zeros, prefix_length, pre
 % the preamble transmitted during the first OFDM symbol and the data
 % transmitted in the subsequent OFDM symbols.
 
+[~, ds_cols] = size(data_symbols);
+if ds_cols ~= 1
+   data_symbols = transpose(data_symbols); % Want a column-vector
+end
 
+[~, ps_cols] = size(preamble_symbols);
+if ps_cols ~= 1
+    preamble_symbols = transpose(preamble_symbols); % Want a column-vector
+end
+
+ofdm_symbol_length = num_carriers;
+ofdm_useful_length = num_carriers - 2*num_zeros;
+
+% Add zeros at the end of the data symbols to make it an integer multiple
+% of the useful capacity of an ofdm symbol
+number_zeros_to_pad = ceil(length(data_symbols) / ofdm_useful_length)*ofdm_symbol_length - length(data_symbols);
+data_symbols_padded = [data_symbols, zeros(1, number_zeros_to_pad)];
+
+% reshape the data symbols into a matrix
+data_symbols_matrix_width = length(data_symbols_padded)/ofdm_useful_length;
+data_symbols_matrix = reshape(data_symbols_padded, ofdm_useful_length, data_symbols_matrix_width);
+
+% Add preamble to data symbols matrix
+data_symbols_preamble_matrix = [preamble_symbols, data_symbols_matrix]; 
+
+% Add lines of 0 on top and bottom of the data sybol matrix for the
+% non-used subcarriers. Note: The +1 is here to take into account the
+% preamble symbol
+ofdm_frame = [ zeros(num_zeros,data_symbols_matrix_width+1); data_symbols_preamble_matrix; zeros(num_zeros,data_symbols_matrix_width+1)];
+
+% Apply IFFT on ofdm frame
+ofdm_frame_ifft = ifft(ofdm_frame);
+
+% Get the part of the matrix to be used as prefix
+cyclic_prefix = ofdm_frame_ifft(end-prefix_length+1:end,:);
+
+% Add cyclic prefix
+tx_signal = [cyclic_prefix ;ofdm_frame_ifft];
+
+end
