69854c68bc720faa10e2f010f02588508fdbf88f
Michael Heiniger <michael.heiniger@epfl.ch>
2016-10-17 22:40:17 +0200

    Unsuccessful tries of channel_est1.m

--- Lab4/code/channel_est1.m
+++ Lab4/code/channel_est1.m
@@ -6,17 +6,13 @@ function lambda = channel_est1(num_carriers, num_zeros, h)
 %   NUM_CARRIERS: number of subcarriers (FFT/IFFT size)
 %   H: channel impulse response; 
 %
-%   LAMBDA: Column vector containing channel coefficients in the frequency domain
+%   LAMBDA: ROW vector containing channel coefficients in the frequency domain
 %   contains num_carriers-2*num_zeros elements
 
-% Get the subcarriers coefficients
-size(h)
-eigenvalues = eig(h);
+v =  [ h(1), zeros(1,num_carriers-length(h)), fliplr(h(2:end))];
+%H = fft(toeplitz([v(1) fliplr(v(2:end))], v), num_carriers) \dftmtx(num_carriers);
+H = eig(toeplitz([v(1) fliplr(v(2:end))], v));
 
-% Removes the first num_zeros and last num_zeros values
-lambda = eigenvalues(num_zeros+1:end-num_zeros);
-
-
- 
- 
- 
+lambda_with_guard_subcarriers = H; %diag(H);
+lambda = transpose(lambda_with_guard_subcarriers(num_zeros+1:end-num_zeros));
+end
